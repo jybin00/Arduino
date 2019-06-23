3b93fb8467b7a473fe578fd75ee59f08af3d5677
yubeen <yubeenjo@Yubeenui-MacBook-Air.local>
2019-06-23 23:00:12 +0900

    oled 주석 추가

--- oled_test_01/oled_test_01.ino
+++ oled_test_01/oled_test_01.ino
@@ -1,19 +1,19 @@
-#include "U8glib.h"
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);
+#include "U8glib.h" // 라이브러리 선언
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE); // oled 모델명에 맞는 함수 선언
    
 void setup() {
 }
    
 void loop() {
-    u8g.firstPage();
+    u8g.firstPage(); // 첫번째 페이지
     do{
-        u8g.setFont(u8g_font_unifont);
-        u8g.setPrintPos(8,15);
-        u8g.print("JYB");
-        u8g.drawStr(0,32, "X.position:");
-        u8g.drawStr(0,43, "Y.position:");
-        u8g.drawEllipse(20,10,20,10,U8G_DRAW_ALL);
-    }while(u8g.nextPage());
+        u8g.setFont(u8g_font_unifont); // OLED 폰트 설정
+        u8g.setPrintPos(8,15); // 프린트할 글자 위치 설정
+        u8g.print("JYB"); // 글자 프린트
+        u8g.drawStr(0,32, "X.position:"); // x위치 프린트
+        u8g.drawStr(0,43, "Y.position:"); // y위치 프린트 
+        u8g.drawEllipse(20,10,20,10,U8G_DRAW_ALL); // 타원 그리기 , x,y,rx,ry,옵션
+    }while(u8g.nextPage()); // 다음 페이지가 계속 나오면 (==1 이면)
 }
 
 
