0fc88c9affd44fff38e0e632174b5df08d8a8049
yubeen <yubeenjo@Yubeenui-MacBook-Air.local>
2019-06-24 00:07:59 +0900

    OLED에 센서값 출력하려면 따로 써줘야 하네? 겁나 귀찮잖아?

--- OLED_DHT22/OLED_DHT22.ino
+++ OLED_DHT22/OLED_DHT22.ino
@@ -19,8 +19,9 @@ void loop() {
         u8g.setFont(u8g_font_unifont); // OLED 폰트 설정
         u8g.setPrintPos(8,15); // 프린트할 글자 위치 설정
         u8g.print("JYB"); // 글자 프린트
-        u8g.drawStr(0,32, "X.position:"); // x위치 프린트
-        u8g.drawStr(0,43, "Y.position:"); // y위치 프린트 
+        u8g.drawStr(0,32, "Temperature:"); // 온도 글자 프린트
+        u8g.drawStr(0,43, "Humiditiy:"); // 습도 글자 프린트
+        u8g.drawStr(0,54, "HeatIndex:"); // 체감 온도 프린트
         u8g.drawEllipse(20,10,20,10,U8G_DRAW_ALL); // 타원 그리기 , x,y,rx,ry,옵션
     }while(u8g.nextPage()); // 다음 페이지가 계속 나오면 (==1 이면)
 }
