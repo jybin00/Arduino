1feeb58e65ff9d9350c01617f81ebb2247f41c04
yubeen <yubeenjo@Yubeenui-MacBook-Air.local>
2019-06-24 09:22:14 +0900

    git add 할 때 파일 단위로 해야할 듯 ㅎㅎ

--- oled_test_01/oled_test_01.ino
+++ oled_test_01/oled_test_01.ino
@@ -3,7 +3,7 @@
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE); // oled 모델명에 맞는 함수 선언
    
 void setup() {
-  Serial.begin(9600); // 시리얼 통신 시작.
+  Serial.begin(9600); // 시리얼 통신 시작. 통신 속도는 9600!
 }
    
 void loop() {
