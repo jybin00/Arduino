aedd864b9479f9086c686246527b4d442a2aaab6
Yubeen Jo <jybin00@naver.com>
2019-06-26 15:01:46 +0900

    포신과 포각 제어

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -3,7 +3,9 @@
 Servo servo_left;  // 왼쪽 서보 모터
 Servo servo_right; // 오른쪽 서보 모터 
 
-int angle = 0;  // 각도
+int angle_left = 0;     // 값을 받아서 모터에 넣어주는 각도
+int angle_right = 0;
+int angle_ini = 0;  // 초기 각도
 int left_motor_pin = 8;  // 왼쪽 모터는 8번 핀
 int right_motor_pin = 7; // 오른쪽 모터는 7번 핀
 
@@ -16,11 +18,41 @@ void setup() {
   servo_left.attach(left_motor_pin);   // 서보 모터 데이터 핀 연결해주기.
   servo_right.attach(right_motor_pin); // 서보 모터 데이터 핀 연겨해주기.
 
+  servo_left.write(angle_ini);   // 서보모터 초기화
+  servo_right.write(angle_ini);  // 서보모터 초기화
+}
+
+void Servo_on()
+{
+  servo_left.attach(left_motor_pin);   // 서보 모터 데이터 핀 연결해주기.
+  servo_right.attach(right_motor_pin); // 서보 모터 데이터 핀 연겨해주기.
+}
+
+void Servo_off()
+{
+  servo_left.detach();   // 서보 모터 데이터 핀 연결해주기.
+  servo_right.detach(); // 서보 모터 데이터 핀 연겨해주기.
 }
 
 void loop() 
 {
-  char angle = Serial.read();
-  servo_left.write(angle);
-  servo_right.write(angle);
+  if(Serial.available())
+  {
+    int angle_left = Serial.parseInt();
+    int angle_right = Serial.parseInt();
+    Serial.print("angle_left: ");
+    Serial.println(angle_left);
+    Serial.print("angle_right: ");
+    Serial.println(angle_right);
+
+    Servo_on();
+    
+    servo_left.write(angle_left);
+    servo_right.write(angle_right);
+    delay(420);
+    
+    Servo_off();
+    delay(1000);
+  }
+ 
 }
