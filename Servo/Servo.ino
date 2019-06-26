ba3e6a0c8715eada935298a9c79c382ae9dfd7d4
Yubeen Jo <jybin00@naver.com>
2019-06-26 15:28:15 +0900

    대포 몸통 만들기 성공! 다음은 버튼으로 조작하기 + 고무줄 장전하기

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -3,8 +3,8 @@
 Servo servo_left;  // 왼쪽 서보 모터
 Servo servo_right; // 오른쪽 서보 모터 
 
-int angle_left = 0;     // 값을 받아서 모터에 넣어주는 각도
-int angle_right = 0;
+int angle_left = 20;     // 값을 받아서 모터에 넣어주는 각도
+int angle_right = 70;    // 20에 20이 기본 세팅
 int angle_ini = 0;  // 초기 각도
 int left_motor_pin = 8;  // 왼쪽 모터는 8번 핀
 int right_motor_pin = 7; // 오른쪽 모터는 7번 핀
@@ -18,8 +18,8 @@ void setup() {
   servo_left.attach(left_motor_pin);   // 서보 모터 데이터 핀 연결해주기.
   servo_right.attach(right_motor_pin); // 서보 모터 데이터 핀 연겨해주기.
 
-  servo_left.write(angle_ini);   // 서보모터 초기화
-  servo_right.write(angle_ini);  // 서보모터 초기화
+  servo_left.write(angle_left);   // 서보모터 초기화
+  servo_right.write(angle_right);  // 서보모터 초기화
 }
 
 void Servo_on()
@@ -48,6 +48,7 @@ void loop()
     Servo_on();
     
     servo_left.write(angle_left);
+    delay(500);
     servo_right.write(angle_right);
     delay(420);
     
