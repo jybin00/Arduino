badebf76ab10cfb6b5f2e9e71655a52f11b986fe
Yubeen Jo <jybin00@naver.com>
2019-06-26 13:13:36 +0900

    fritzing도 보일려나?

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -1,3 +1,11 @@
+#include <Servo.h> // 서보 모터 이용을 위한 라이브러
+
+Servo servo_left;  // 왼쪽 서보 모터
+Servo servo_right; // 오른쪽 서보 모터 
+
+int left_motor = 8;  // 왼쪽 모터는 8번 핀
+int right_motor = 7; // 오른쪽 모터는 7번 핀
+
 void setup() {
   // put your setup code here, to run once:
 
