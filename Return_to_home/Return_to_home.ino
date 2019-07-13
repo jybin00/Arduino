c02eea802aa966350573fed2a8ec2c6d4d8a148e
Yubeen Jo <jybin00@naver.com>
2019-07-13 21:09:37 +0900

    OLED 테스트

--- Return_to_home/Return_to_home.ino
+++ Return_to_home/Return_to_home.ino
@@ -3,9 +3,13 @@
 // 작성자 : 승민,영준,하영,유빈
 //
 
+#include <SSD1306Wire.h>
 
-// 스피드 설정 0.2초 마다 1스텝씩 이동
-#define DEFAULT_SPEED 20 
+SSD1306Wire display(0x3c, 21,22);
+
+
+// 스피드 설정 0.15초 마다 1스텝씩 이동
+#define DEFAULT_SPEED 60
 
 // 모터 방향 설정
 #define FORWARD 1
@@ -21,12 +25,21 @@ unsigned long LeftMotorTimer, RightMotorTimer, systemTimer;
 #define LeftStep(a,b,c,d) digitalWrite(19,a); digitalWrite(18,b); digitalWrite(4,c); digitalWrite(0,d);
 
 int LeftMotorDir, RightMotorDir;  // 왼쪽 모터와 오른쪽 모터 방향 변수
-int LeftMotorStepIndex = 0;  // 왼쪽 모터의 스텝수 저장 변수
-int RightMotorStepIndex = 0; // 오른쪽 모터의 스텝수 저장 변수
+
+int LeftMotorStepIndex = 0;  // 왼쪽 모터 제어용 인덱스
+int RightMotorStepIndex = 0; // 오른쪽 모터 제어용 인덱스
+
+int LeftStepIndex = 0;  // 왼쪽 모터 스텝 저장용 변수
+int RightStepIndex = 0; // 오른쪽 모터 스텝 저장용 변수
 
 int Distance[3];  // 거리 저장을 위한 배열
 
 void setup() {
+  Serial.begin(115200);
+
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
 
 ///// 스텝모터 초기 설정
   pinMode(26, OUTPUT);
@@ -55,10 +68,12 @@ void LeftMotorStep() // 왼쪽 모터 동작을 위한 함수
     if(LeftMotorStepIndex == 3) 
     {
       LeftMotorStepIndex = 0; // 인덱스가 3이면 0으로 초기화
+      LeftStepIndex++;
     }
     else 
     {
       LeftMotorStepIndex++;
+      LeftStepIndex++;
     }
   }
   else if(LeftMotorDir == BACKWARD)
@@ -140,6 +155,23 @@ void RightMotorStep() // 오른쪽 모터 제어를 위한 함수
   }
 }
 
+void drawFontFaceDemo() {
+  // clear the display
+  display.clear();
+    // Font Demo1
+    // create more fonts at http://oleddisplay.squix.ch/
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 0, "Hello world");
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(0, 10, "Hello world");
+    display.setFont(ArialMT_Plain_24);
+    display.drawString(0, 26, "Hello world");
+  // write the buffer to the display
+  display.display();
+}
+
+
 void loop() 
 { // 스피드로 설정한 시간이 지날 때마다 실행
   if(millis() >= LeftMotorTimer + DEFAULT_SPEED)  
@@ -154,5 +186,13 @@ void loop()
     RightMotorStep();
   }
 
+  if(millis() >= systemTimer + 1000)
+  {
+    systemTimer = millis();
+    Serial.println(LeftStepIndex);
+  }
+
+  drawFontFaceDemo();
+  delay(1000);
 
 }
