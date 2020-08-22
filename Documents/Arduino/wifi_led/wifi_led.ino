a3f94363c1895238f1fa9f2e78a8a64c6b265eab
Yubeen Jo <jybin00@naver.com>
2020-08-22 20:40:46 +0900

    nodemcu_wifi_led

---
+++ Documents/Arduino/wifi_led/wifi_led.ino
@@ -0,0 +1,119 @@
+#include <ESP8266WiFi.h> // gkdnlgkdnl
+
+#define PIN_LED_RED D0
+#define PIN_LED_BLUE D1
+#define PIN_LED_YELLOW D2
+
+const char* ssid = "Up to the SKY";
+const char* password = "1402036768";
+
+WiFiServer server(80);
+
+void setup() {
+  pinMode(PIN_LED_RED, OUTPUT);
+  digitalWrite(PIN_LED_RED, LOW);
+  pinMode(PIN_LED_BLUE, OUTPUT);
+  digitalWrite(PIN_LED_BLUE, LOW);
+  pinMode(PIN_LED_YELLOW, OUTPUT);
+  digitalWrite(PIN_LED_YELLOW, LOW);
+  
+  Serial.begin(115200);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  server.begin();
+  Serial.println("Server started");
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if(!client) return;
+  
+  Serial.println("새로운 클라이언트");
+  client.setTimeout(5000);
+  
+  String request = client.readStringUntil('\r');
+  Serial.println("request: ");
+  Serial.println(request);
+
+  if(request.indexOf("/ledoff_r") != -1) {
+    digitalWrite(PIN_LED_RED, LOW);
+  }
+  else if(request.indexOf("/ledon_r") != -1) {
+    digitalWrite(PIN_LED_RED, HIGH);
+  }
+  else if(request.indexOf("/ledoff_b") != -1) {
+    digitalWrite(PIN_LED_BLUE, LOW);
+  }
+  else if(request.indexOf("/ledon_b") != -1) {
+    digitalWrite(PIN_LED_BLUE, HIGH);
+  }
+  else if(request.indexOf("/ledoff_y") != -1) {
+    digitalWrite(PIN_LED_YELLOW, LOW);
+  }
+  else if(request.indexOf("/ledon_y") != -1) {
+    digitalWrite(PIN_LED_YELLOW, HIGH);
+  }
+  else {
+    Serial.println("invalid request");
+    digitalWrite(PIN_LED_RED, digitalRead(PIN_LED_RED));
+    digitalWrite(PIN_LED_BLUE, digitalRead(PIN_LED_BLUE));
+    digitalWrite(PIN_LED_YELLOW, digitalRead(PIN_LED_YELLOW));
+  }
+
+  while(client.available()) {
+    client.read();
+  }
+
+  client.print("HTTP/1.1 200 OK");
+  client.print("Content-Type: text/html\r\n\r\n");
+  client.print("<!DOCTYPE HTML>");
+  client.print("<html>");
+  client.print("<head>"); 
+  client.print("<meta&nbsp;charset=\"UTF-8\">");
+  client.print("<title>LED Control Webpage</title>");
+  client.print("</head>");
+  client.print("<body>");
+  client.print("<h2>LED Control Webpage</h2>");
+  client.print("<a href='/ledon_r'><button>LED RED ON</button></a> &emsp;&emsp;&emsp;");
+  client.print("<a href='/ledoff_r'><button>LED RED OFF</button></a>  ");
+  client.print("<br>");
+  client.print("<br>");
+  
+  client.print("<a href='/ledon_b'>LED BLUE ON</a> &emsp;&emsp; ");
+  client.print("<a href='/ledoff_b'>LED BLUE OFF</a>  ");
+  client.print("<br>");
+  client.print("<br>");
+  
+  client.print("<a href='/ledon_y'>LED YELLOW ON</a> &emsp; ");
+  client.print("<a href='/ledoff_y'>LED YELLOW OFF</a>  ");
+  client.print("<br>");
+  
+  client.print("<br>");
+  client.print("LED Status RED : ");
+  client.print((digitalRead(PIN_LED_RED)) ? "ON" : "OFF");
+  client.print("<br>");
+  client.print("<br>");
+  client.print("LED Status BLUE : ");
+  client.print((digitalRead(PIN_LED_BLUE)) ? "ON" : "OFF");
+  client.print("<br>");
+  client.print("<br>");
+  client.print("LED Status YELLOW : ");
+  client.print((digitalRead(PIN_LED_YELLOW)) ? "ON" : "OFF");
+  client.print("</body>");
+  client.print("</html>");
+
+  Serial.println("클라이언트 연결 해제");
+}
+
