93cd5c78ef9558cb4fae9b4e67fa6e3933355c41
yubeen <jybin00@korea.ac.kr>
2022-07-24 14:20:57 +0900

    Cansat gps+oled+bmp test file

---
+++ 2022_Cansat_sensor_communication/gps_test.ino
@@ -0,0 +1,80 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+#include "TinyGPSPlus.h"
+#include <SoftwareSerial.h>
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device RX, TX
+SoftwareSerial ss(13, 15);
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(128, 64, &Wire, 5);
+
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BMP3XX bmp;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+    Serial.println(F("Could not find a valid BMP3 sensor, check wiring!"));
+    while (1);
+  }
+
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    while(1); // Don't proceed, loop forever
+  }
+  
+  ss.begin(9600);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.clearDisplay();
+
+}
+
+void loop() {
+  if (! bmp.performReading()) {
+  Serial.println(F("Failed to perform reading :("));
+  return;  }
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.print("Temp : "); display.print(bmp.temperature); display.println(" C");
+  display.print("Press = ");display.print(bmp.pressure / 100.0); display.println(" hPa");
+  display.print("Alti = "); display.print(bmp.readAltitude(SEALEVELPRESSURE_HPA)); display.println(" m");
+  
+  display.setCursor(0,40);
+  
+  
+  if(ss.available() > 0){
+    if (gps.encode(ss.read())){
+      display.println(F("Location: "));
+      if (gps.location.isValid()){
+        display.println(gps.location.lat(), 2);
+        display.println(F(","));
+        display.print(gps.location.lng(), 2);
+      }
+      else
+      {
+        display.print(F("No signal"));
+      } 
+    }
+  }
+  else
+    display.println(F("Invalid")); 
+
+  display.display();
+  delay(2000);
+}
