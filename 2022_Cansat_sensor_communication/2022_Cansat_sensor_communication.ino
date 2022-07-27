b1eb40336bbdc7909b69a781912d0bb6c6991ec8
yubeen <jybin00@korea.ac.kr>
2022-07-27 17:18:51 +0900

    many changes :( I can't oled

--- 2022_Cansat_sensor_communication/2022_Cansat_sensor_communication.ino
+++ 2022_Cansat_sensor_communication/2022_Cansat_sensor_communication.ino
@@ -1,83 +1,179 @@
 #include <SPI.h>
 #include <Wire.h>
-// oled libraries
-#include "SSD1306Wire.h"
+
+// oled libraries //
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 4);
+
 // BMP390 libraries
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
+
+// DHT22 libraries
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
 // GPS libraries
 #include "TinyGPSPlus.h"
-#include <SoftwareSerial.h>
+
+// MPU9250
+#include "MPU9250.h"
+MPU9250 mpu;
+uint8_t device_count = 0;
+uint8_t addrs[7] = {0};
+
+static const uint32_t GPSBaud = 9600;
+
+float temp, humi, pressure, alti;
+float longitude, latitude;
+int day1, month1, year1;
 
 // The TinyGPSPlus object
 TinyGPSPlus gps;
 
-// The serial connection to the GPS device RX, TX
-SoftwareSerial ss(13, 15);
-
-// Initialize the OLED display using Wire library
-SSD1306Wire  display(0x3c, D3, D5);
-
-
 #define SEALEVELPRESSURE_HPA (1013.25)
+
 Adafruit_BMP3XX bmp;
 
 void setup() {
   Serial.begin(115200);
-  display.init();
+  Wire.begin();
+  Wire.setClock(100000);
+  while (!Serial);
+  Serial.println(F("2022_Cansat_sensor_communication test"));
+  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  Serial.println(F("Oled found."));
 
   if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
+  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
     Serial.println(F("Could not find a valid BMP3 sensor, check wiring!"));
     while (1);
   }
-
-  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
-
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    while(1); // Don't proceed, loop forever
-  }
+  Serial.println(F("bmp sensor found."));
   
-  ss.begin(9600);
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_2X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_25_HZ);
+
+  
+  if (!mpu.setup(0x68)) {  // change to your own address
+      while (1) {
+          Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+          delay(5000);
+      }
+  }
+  Serial.println(F("MPU9250 found."));
+  
+  Serial3.begin(GPSBaud);
+
+  Serial.println(TinyGPSPlus::libraryVersion());
+  delay(1000);
+  Serial.println(F("GPS ready"));
+
+  dht.begin();
+  Serial.println(F("dht begin."));
   display.clearDisplay();
 
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  mpu.setFilterIterations(10);
 }
 
 void loop() {
-  if (! bmp.performReading()) {
-  Serial.println(F("Failed to perform reading :("));
-  return;  }
   display.clearDisplay();
-  display.setCursor(0,0);
-  display.print("Temp : "); display.print(bmp.temperature); display.println(" C");
-  display.print("Press = ");display.print(bmp.pressure / 100.0); display.println(" hPa");
-  display.print("Alti = "); display.print(bmp.readAltitude(SEALEVELPRESSURE_HPA)); display.println(" m");
+  bmp.performReading();
   
-  display.setCursor(0,40);
-  
-  
-  if(ss.available() > 0){
-    if (gps.encode(ss.read())){
-      display.println(F("Location: "));
-      if (gps.location.isValid()){
-        display.println(gps.location.lat(), 2);
-        display.println(F(","));
-        display.print(gps.location.lng(), 2);
+  if (mpu.available()) {
+    mpu.update();
+    static uint32_t prev_ms = millis();
+    if (millis() > prev_ms + 20) {
+        print_roll_pitch_yaw();
+        prev_ms = millis();
       }
-      else
-      {
-        display.print(F("No signal"));
-      } 
+  }
+
+  if (bmp.temperature) {
+    static uint32_t prev_ms = millis();
+    if (millis() > prev_ms + 2005) {
+      temp = bmp.temperature;
+      pressure = bmp.pressure/100;
+      alti = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+      //humi = dht.readHumidity();
+      prev_ms = millis();
+      }
+  }  
+
+  
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (Serial3.available() > 0){
+    if (gps.encode(Serial3.read())){
+      displayInfo();
     }
   }
-  else
-    display.println(F("Invalid")); 
 
+  display.setCursor(0, 0);
+  display.print("T:" + String(temp) + "*C"); 
+  display.setCursor(64, 0);
+  display.println("H:" + String(humi) + "%");
+  display.setCursor(0, 10);
+  display.println("P:" + String(pressure) + " hPa"); 
+  display.setCursor(0, 20);
+  display.println("Alti:" + String(alti) + " m");
+
+  display.setCursor(0,30);
+  display.print(F("Location: "));
+  
+  display.print(latitude, 6);
+  display.println(F(","));
+  display.setCursor(0,40);
+  display.print(longitude, 6);
+  
+  display.setCursor(0,50);
+  display.print(gps.date.month());
+  display.print(F("/"));
+  display.print(gps.date.day());
+  display.print(F("/"));
+  display.print(gps.date.year());
+  
   display.display();
-  delay(2000);
+ 
+}
+
+void displayInfo()
+{
+  if (gps.location.isValid())
+  {
+    latitude = gps.location.lat();
+    longitude = gps.location.lng();
+  }
+  if (gps.date.isValid())
+  {
+    month1 = gps.date.month();
+    day1 = gps.date.day();
+    year1 = gps.date.year();
+  }
+}
+
+void print_roll_pitch_yaw() {
+    Serial.print("Yaw, Pitch, Roll: ");
+    Serial.print(mpu.getYaw(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getPitch(), 2);
+    Serial.print(", ");
+    Serial.println(mpu.getRoll(), 2);
 }
