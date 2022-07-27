29b73cda4dea013bcd3bb07f18692535059c7702
yubeen <jybin00@korea.ac.kr>
2022-07-27 17:30:11 +0900

    oled libraries deleted.

--- 2022_Cansat_sensor_communication/2022_Cansat_sensor_communication.ino
+++ 2022_Cansat_sensor_communication/2022_Cansat_sensor_communication.ino
@@ -1,11 +1,13 @@
 #include <SPI.h>
 #include <Wire.h>
 
+/*
 // oled libraries //
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+*/
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 4);
