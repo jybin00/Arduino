b599590bcd0bd83f04f32a3684869a5d8eff381c
yubeen <jybin00@korea.ac.kr>
2022-09-25 19:38:59 +0900

    변경 사항

--- 2022_Cansat_sensor_communication/nodemcu_ver/nodemcu_ver.ino
+++ 2022_Cansat_sensor_communication/nodemcu_ver/nodemcu_ver.ino
@@ -2,7 +2,7 @@
 #include <Wire.h>
 
 #include <RF24.h>
-RF24 radio(42, 44); // SPI통신을 위한 (CE, CSN) 핀 선언
+RF24 radio(10, 9); // SPI통신을 위한 (CE, CSN) 핀 선언
 
 const byte address[6] = "99074"; // 송신기와 수신기가 동일한 값으로 주소 설정함(5자리)
 
@@ -12,7 +12,7 @@ typedef struct CANSAT_Info
   short altitudee;
   short Roll, Pitch, Yaw;
   short humidity, temperature;
-  char test[2] = "hi";
+  char test[2] = {'h', 'i'};
 }Info;
 
 // BMP390 libraries
@@ -21,12 +21,13 @@ typedef struct CANSAT_Info
 
 // DHT22 libraries
 #include "DHT.h"
-#define DHTPIN 3
+#define DHTPIN D0
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
 // GPS libraries
 #include "TinyGPSPlus.h"
+#include "SoftwareSerial.h"
 
 // MPU9250
 #include "MPU9250.h"
@@ -36,94 +37,97 @@ static const uint32_t GPSBaud = 9600;
 
 // The TinyGPSPlus object
 TinyGPSPlus gps;
+SoftwareSerial ss(D7, D6); //Rx, Tx
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BMP3XX bmp;
 
-//bool rf_on_off = true;
+bool rf_on_off = true;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
+  pinMode(D5, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   Wire.begin();
-  while (!Serial);
-  //Serial.println(F("2022_Cansat_sensor_communication test"));
-
-  //Serial.println(F("RF module setting...."));
+  delay(100);
+  Serial.println(F("2022_Cansat_sensor_communication test"));
+  Serial.println(F("RF module setting...."));
   radio.begin();
   radio.openWritingPipe(address); // 데이터를 보낼 수신 주소를 설정
   radio.setDataRate(RF24_250KBPS);
   radio.setChannel(14);
-  radio.setPALevel(RF24_PA_HIGH); // 송신거리에 따른, 전원공급 파워레벨 설정
-  //Serial.println(F("RF module setting end!"));
+  radio.setPALevel(RF24_PA_LOW); // 송신거리에 따른, 전원공급 파워레벨 설정
+  Serial.println(F("RF module setting end!"));
+
 
   if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
-  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
-  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    while(1){
     Serial.println(F("Could not find a valid BMP3 sensor, check wiring!"));
-    while (1);
+    delay(1000);
+    }
   }
-  //Serial.println(F("bmp sensor found."));
+  Serial.println(F("bmp sensor found."));
   
   // Set up oversampling and filter initialization
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_4X);
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_2X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_25_HZ);
-  mpu.selectFilter(QuatFilterSel::MADGWICK);
+
+  
   if (!mpu.setup(0x68)) {  // change to your own address
       while (1) {
           Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
-          delay(5000);
+          delay(1000);
       }
   }
-  mpu.setFilterIterations(15);
-  mpu.selectFilter(QuatFilterSel::MADGWICK);
-  mpu.setMagneticDeclination(-8.52);
-  mpu.setGyroBias(-0.98, 0.11, -5.66);
-  mpu.setAccBias(-132.73, -607.22, 550.18);
-  mpu.setMagBias(-290.34, 100.69, -668.66);
-  mpu.setMagScale(1.24, 1.34, 0.69);
-  //Serial.println(F("MPU9250 found."));
+  Serial.println(F("MPU9250 found."));
   
-  Serial3.begin(GPSBaud);
+  ss.begin(GPSBaud);
 
+  Serial.println(TinyGPSPlus::libraryVersion());
   delay(1000);
-  //Serial.println(F("GPS ready"));
+  Serial.println(F("GPS ready"));
 
   dht.begin();
-  //Serial.println(F("dht begin."));
+  Serial.println(F("dht begin."));
 
+  mpu.selectFilter(QuatFilterSel::MADGWICK);
+  mpu.setFilterIterations(15);
+  mpu.setMagneticDeclination(-8.52);
+  mpu.calibrateAccelGyro();
+  mpu.calibrateMag();
   radio.stopListening();  // 모듈을 송신기로 설정
 }
 
 void loop() {
   Info info;
   bmp.performReading();
-/*
+  Serial.println("hi");
+
   if(rf_on_off == true){  // rf 수신
-    if(digitalRead(31) == LOW){
+    if(digitalRead(D5) == HIGH){
       rf_on_off = false;
       radio.startListening();
-      digitalWrite(13,LOW);
+      digitalWrite(LED_BUILTIN,LOW);
       Serial.println("start Listening");
       delay(500);
       }
   }
   else if(rf_on_off == false){   // rf 송신
-    if(digitalRead(31) == LOW){
+    if(digitalRead(D5) == HIGH){
       rf_on_off = true;
       radio.stopListening();
       Serial.println("start Shooting");
-      digitalWrite(13,HIGH);
+      digitalWrite(LED_BUILTIN,HIGH);
       delay(500);
       }
   }
-  */
+  
   if (mpu.update()) {
     static uint32_t prev_ms = millis();
-    if (millis() > prev_ms + 25) {
+    if (millis() > prev_ms + 10) {
         short roll = mpu.getRoll()*100;
         short yaw = mpu.getYaw()*100;
         short pitch = mpu.getPitch()*100;
@@ -139,30 +143,24 @@ void loop() {
     if (millis() > prev_ms + 1005) {
       info.temperature = bmp.temperature;
       info.altitudee = bmp.readAltitude(SEALEVELPRESSURE_HPA);
-      //info.humidity = short(bmp.pressure);
       info.humidity = dht.readHumidity();
       prev_ms = millis();
       }
   }  
 
   // This sketch displays information every time a new sentence is correctly encoded.
-  while (Serial3.available() > 0){
-    if (gps.encode(Serial3.read())){
+  while (ss.available() > 0){
+    if (gps.encode(ss.read())){
       if (gps.location.isValid())
       {
         info.latitude = gps.location.lat();
         info.longitude = gps.location.lng();
       }
     }
-    else{
-      info.latitude = 37.58277;
-      info.longitude = 127.02666;
-    }
   }
   bool report = radio.write(&info, sizeof(Info));
   if(report){
-    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println(F("Transmission successful!"));
   }
-  else{ digitalWrite(LED_BUILTIN, LOW); }
  
 }
--- 2022_Cansat_sensor_communication/uno_ver/uno_ver.ino
+++ 2022_Cansat_sensor_communication/uno_ver/uno_ver.ino
@@ -1,19 +1,6 @@
+//////////////////////////******* Libraries ********
 #include <SPI.h>
 #include <Wire.h>
-
-#include <RF24.h>
-RF24 radio(42, 44); // SPI통신을 위한 (CE, CSN) 핀 선언
-
-const byte address[6] = "99074"; // 송신기와 수신기가 동일한 값으로 주소 설정함(5자리)
-
-typedef struct CANSAT_Info
-{
-  float latitude, longitude;
-  short altitudee;
-  short Roll, Pitch, Yaw;
-  short humidity, temperature;
-  char test[2] = "hi";
-}Info;
 
 // BMP390 libraries
 #include <Adafruit_Sensor.h>
@@ -21,89 +8,112 @@ typedef struct CANSAT_Info
 
 // DHT22 libraries
 #include "DHT.h"
-#define DHTPIN 3
-#define DHTTYPE DHT22
-DHT dht(DHTPIN, DHTTYPE);
 
 // GPS libraries
 #include "TinyGPSPlus.h"
+#include "SoftwareSerial.h"
 
 // MPU9250
 #include "MPU9250.h"
-MPU9250 mpu;
 
-static const uint32_t GPSBaud = 9600;
+#include <RF24.h>
+////////////////////////////////////////////////////////
+
+typedef struct CANSAT_Info
+{
+  float latitude, longitude;
+  short altitudee;
+  short Roll, Pitch, Yaw;
+  short humidity, temperature;
+  char test[2] = {'h', 'i'};
+}Info;
+
+RF24 radio(7, 8); // SPI통신을 위한 (CE, CSN) 핀 선언
+const byte address[6] = "99074"; // 송신기와 수신기가 동일한 값으로 주소 설정함(5자리)
+
+#define DHTPIN 5
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+MPU9250 mpu;
 
 // The TinyGPSPlus object
 TinyGPSPlus gps;
+SoftwareSerial ss(3,4); // RXPin, TXPin
+static const uint32_t GPSBaud = 9600;
 
 #define SEALEVELPRESSURE_HPA (1013.25)
-
 Adafruit_BMP3XX bmp;
 
-//bool rf_on_off = true;
+bool rf_on_off = true;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
+  pinMode(6, INPUT);
+  pinMode(13, OUTPUT);
   Wire.begin();
   while (!Serial);
-  //Serial.println(F("2022_Cansat_sensor_communication test"));
+  Serial.println(F("2022_Cansat_sensor_communication test"));
 
-  //Serial.println(F("RF module setting...."));
+  Serial.println(F("RF module setting...."));
   radio.begin();
   radio.openWritingPipe(address); // 데이터를 보낼 수신 주소를 설정
   radio.setDataRate(RF24_250KBPS);
   radio.setChannel(14);
-  radio.setPALevel(RF24_PA_HIGH); // 송신거리에 따른, 전원공급 파워레벨 설정
-  //Serial.println(F("RF module setting end!"));
+  radio.setPALevel(RF24_PA_LOW); // 송신거리에 따른, 전원공급 파워레벨 설정
+  Serial.println(F("RF module setting end!"));
+  
+  /*
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  Serial.println(F("Oled found."));
+  */
 
   if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
-  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
-  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
     Serial.println(F("Could not find a valid BMP3 sensor, check wiring!"));
     while (1);
   }
-  //Serial.println(F("bmp sensor found."));
+  Serial.println(F("bmp sensor found."));
   
   // Set up oversampling and filter initialization
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_4X);
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_2X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_25_HZ);
-  mpu.selectFilter(QuatFilterSel::MADGWICK);
+
+  
   if (!mpu.setup(0x68)) {  // change to your own address
       while (1) {
           Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
           delay(5000);
       }
   }
-  mpu.setFilterIterations(15);
-  mpu.selectFilter(QuatFilterSel::MADGWICK);
-  mpu.setMagneticDeclination(-8.52);
-  mpu.setGyroBias(-0.98, 0.11, -5.66);
-  mpu.setAccBias(-132.73, -607.22, 550.18);
-  mpu.setMagBias(-290.34, 100.69, -668.66);
-  mpu.setMagScale(1.24, 1.34, 0.69);
-  //Serial.println(F("MPU9250 found."));
+  Serial.println(F("MPU9250 found."));
   
-  Serial3.begin(GPSBaud);
+  ss.begin(GPSBaud);
 
+  Serial.println(TinyGPSPlus::libraryVersion());
   delay(1000);
-  //Serial.println(F("GPS ready"));
+  Serial.println(F("GPS ready"));
 
   dht.begin();
-  //Serial.println(F("dht begin."));
+  Serial.println(F("dht begin."));
 
+  mpu.setFilterIterations(15);
+  mpu.setMagneticDeclination(-8.52);
+  mpu.calibrateAccelGyro();
+  mpu.calibrateMag();
   radio.stopListening();  // 모듈을 송신기로 설정
 }
 
 void loop() {
   Info info;
   bmp.performReading();
-/*
+
   if(rf_on_off == true){  // rf 수신
-    if(digitalRead(31) == LOW){
+    if(digitalRead(6) == LOW){
       rf_on_off = false;
       radio.startListening();
       digitalWrite(13,LOW);
@@ -112,7 +122,7 @@ void loop() {
       }
   }
   else if(rf_on_off == false){   // rf 송신
-    if(digitalRead(31) == LOW){
+    if(digitalRead(6) == LOW){
       rf_on_off = true;
       radio.stopListening();
       Serial.println("start Shooting");
@@ -120,10 +130,10 @@ void loop() {
       delay(500);
       }
   }
-  */
+  
   if (mpu.update()) {
     static uint32_t prev_ms = millis();
-    if (millis() > prev_ms + 25) {
+    if (millis() > prev_ms + 10) {
         short roll = mpu.getRoll()*100;
         short yaw = mpu.getYaw()*100;
         short pitch = mpu.getPitch()*100;
@@ -139,30 +149,24 @@ void loop() {
     if (millis() > prev_ms + 1005) {
       info.temperature = bmp.temperature;
       info.altitudee = bmp.readAltitude(SEALEVELPRESSURE_HPA);
-      //info.humidity = short(bmp.pressure);
       info.humidity = dht.readHumidity();
       prev_ms = millis();
       }
   }  
 
   // This sketch displays information every time a new sentence is correctly encoded.
-  while (Serial3.available() > 0){
-    if (gps.encode(Serial3.read())){
+  while (ss.available() > 0){
+    if (gps.encode(ss.read())){
       if (gps.location.isValid())
       {
         info.latitude = gps.location.lat();
         info.longitude = gps.location.lng();
       }
     }
-    else{
-      info.latitude = 37.58277;
-      info.longitude = 127.02666;
-    }
   }
   bool report = radio.write(&info, sizeof(Info));
   if(report){
-    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println(F("Transmission successful!"));
   }
-  else{ digitalWrite(LED_BUILTIN, LOW); }
  
 }
